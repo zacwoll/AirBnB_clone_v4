ca839ce6680a2b55839727897c6d663ff26c2d3c
zacwoll <zacwoll@gmail.com>
2020-09-27 02:18:32 +0000

    I finally got the available removed when the api/v1/status fails

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -16,10 +16,12 @@ $(document).ready(function () {
   });
 });
 
-$.get('http://localhost:5001/api/v1/status/', function (data, status) {
-  if (data.status === 'OK') {
-    $('#api_status').addClass('available');
-  } else {
+$.get('http://localhost:5001/api/v1/status/')
+  .done(function (data, status) {
+    if (data.status === 'OK') {
+      $('#api_status').addClass('available');
+    }
+  })
+  .fail(function () {
     $('#api_status').removeClass('available');
-  }
-});
+  });
