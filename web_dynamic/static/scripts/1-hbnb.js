709e0af25feaca43dd85dd805a370c20f66a59a8
Samuel Pomeroy <1690@holbertonschool.com>
2020-09-27 00:24:36 +0000

    first working build minus ellipsis overflow

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,11 +1,15 @@
-$(function () {
+$(document).ready(function () {
   const amenities = {};
   $('input[type="checkbox"]').click(function () {
     $(this).each(function () {
       if (this.checked) {
-	console.log(this);
-	//amenities[this.id] = this;
+	//console.log($(this).data('id'));
+	amenities[$(this).data('id')] = $(this).data('name');
+      } else {
+	delete amenities[$(this).data('id')];
       }
+      $('.amenities h4').text(Object.values(amenities).join(', '));
+      //console.log(Object.values(amenities));
     });
   });
 });
