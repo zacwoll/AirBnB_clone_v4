3679e4e90ae55432df15da38f77f4ac292c1e48f
Samuel Pomeroy <1690@holbertonschool.com>
2020-09-27 00:28:42 +0000

    remove log tracers

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -3,13 +3,11 @@ $(document).ready(function () {
   $('input[type="checkbox"]').click(function () {
     $(this).each(function () {
       if (this.checked) {
-	//console.log($(this).data('id'));
 	amenities[$(this).data('id')] = $(this).data('name');
       } else {
 	delete amenities[$(this).data('id')];
       }
       $('.amenities h4').text(Object.values(amenities).join(', '));
-      //console.log(Object.values(amenities));
     });
   });
 });
