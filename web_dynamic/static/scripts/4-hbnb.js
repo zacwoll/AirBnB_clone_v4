fd5916814e890a925ef4f1eadb0597ad876d45dd
zacwoll <zacwoll@gmail.com>
2020-09-28 18:04:26 +0000

    semistandard pass

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -10,44 +10,43 @@ $(document).ready(function () {
         delete amenities[$(this).data('id')];
       }
     });
-	$('.amenities h4').html(Object.values(amenities).join(', ') || '&nbsp;');
+    $('.amenities h4').html(Object.values(amenities).join(', ') || '&nbsp;');
   });
 
-	$('button').click(() => {
-		const data = {
-			amenities: Object.keys(amenities)
-		}
-		$('.places').empty();
-		$.ajax({
-			method: 'POST',
-			url: 'http://localhost:5001/api/v1/places_search/',
-			data: JSON.stringify(data),
-			contentType: 'application/json; charset=utf-8'
-		})
-		.done(function (data, status) {
-				let placeTemplate;
-				for (const place of data) {
-					placeTemplate = `
-				<article>
-					<div class="title_box">
-						<h2>${place.name}</h2>
-						<div class="price_by_night">$${place.price_by_night}</div>
-					</div>
-					<div class="information">
-						<div class="max_guest">${place.max_guest} Guest${place.max_guest !== 1 ? 's' : ''}</div>
-						<div class="number_rooms">${place.number_rooms} Bedroom${place.number_rooms !== 1 ? 's' : ''}</div>
-						<div class="number_bathrooms">${place.number_bathrooms} Bathroom${place.number_bathrooms !== 1 ? 's' : ''}</div>
-					</div>
-					<!-- user div removed -->
-					<div class="description">
-						${place.description}
-					</div>
-				</article>
-								`;
-				$('.places').append(placeTemplate);
-			}
-		});
-	});
+  $('button').click(() => {
+    const data = {
+      amenities: Object.keys(amenities)
+    };
+    $('.places').empty();
+    $.ajax({
+      method: 'POST',
+      url: 'http://localhost:5001/api/v1/places_search/',
+      data: JSON.stringify(data),
+      contentType: 'application/json; charset=utf-8'
+    })
+      .done(function (data, status) {
+        let placeTemplate;
+        for (const place of data) {
+          placeTemplate = `
+<article>
+<div class="title_box">
+<h2>${place.name}</h2>
+<div class="price_by_night">$${place.price_by_night}</div>
+</div>
+<div class="information">
+<div class="max_guest">${place.max_guest} Guest${place.max_guest !== 1 ? 's' : ''}</div>
+<div class="number_rooms">${place.number_rooms} Bedroom${place.number_rooms !== 1 ? 's' : ''}</div>
+<div class="number_bathrooms">${place.number_bathrooms} Bathroom${place.number_bathrooms !== 1 ? 's' : ''}</div>
+</div>
+<div class="description">
+${place.description}
+</div>
+</article>
+`;
+          $('.places').append(placeTemplate);
+        }
+      });
+  });
 });
 
 $.get('http://localhost:5001/api/v1/status/')
