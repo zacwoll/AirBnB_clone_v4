c523ee7ac1ac2b3366961419cdf212bbb9ebde68
Samuel Pomeroy <1690@holbertonschool.com>
2020-09-28 20:40:28 +0000

    semistandard t1

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,11 +1,13 @@
+const $ = window.jQuery;
+
 $(document).ready(function () {
   const amenities = {};
   $('input[type="checkbox"]').click(function () {
     $(this).each(function () {
       if (this.checked) {
-	amenities[$(this).data('id')] = $(this).data('name');
+        amenities[$(this).data('id')] = $(this).data('name');
       } else {
-	delete amenities[$(this).data('id')];
+        delete amenities[$(this).data('id')];
       }
       $('.amenities h4').text(Object.values(amenities).join(', '));
     });
