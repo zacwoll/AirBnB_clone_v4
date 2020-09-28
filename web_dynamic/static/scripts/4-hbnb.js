5006963cb77a5b64b27d9b5b9233bcabdbff9303
zacwoll <zacwoll@gmail.com>
2020-09-28 18:09:25 +0000

    static/scripts/4-hbnb.js

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -29,18 +29,18 @@ $(document).ready(function () {
         for (const place of data) {
           placeTemplate = `
 <article>
-<div class="title_box">
-<h2>${place.name}</h2>
-<div class="price_by_night">$${place.price_by_night}</div>
-</div>
-<div class="information">
-<div class="max_guest">${place.max_guest} Guest${place.max_guest !== 1 ? 's' : ''}</div>
-<div class="number_rooms">${place.number_rooms} Bedroom${place.number_rooms !== 1 ? 's' : ''}</div>
-<div class="number_bathrooms">${place.number_bathrooms} Bathroom${place.number_bathrooms !== 1 ? 's' : ''}</div>
-</div>
-<div class="description">
-${place.description}
-</div>
+  <div class="title_box">
+    <h2>${place.name}</h2>
+    <div class="price_by_night">$${place.price_by_night}</div>
+  </div>
+    <div class="information">
+    <div class="max_guest">${place.max_guest} Guest${place.max_guest !== 1 ? 's' : ''}</div>
+    <div class="number_rooms">${place.number_rooms} Bedroom${place.number_rooms !== 1 ? 's' : ''}</div>
+    <div class="number_bathrooms">${place.number_bathrooms} Bathroom${place.number_bathrooms !== 1 ? 's' : ''}</div>
+  </div>
+  <div class="description">
+    ${place.description}
+  </div>
 </article>
 `;
           $('.places').append(placeTemplate);
